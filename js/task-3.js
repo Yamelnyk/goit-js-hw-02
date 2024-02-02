fb7bed2a979e3a2034c3d93782bf59aa2aae9e39
Yamelnyk <yachkana@ukr.net>
2024-02-02 15:57:16 +0200

    tasks 3,4

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function checkForSpam(message) {
-    const lowerCaseMessage = message.toLowerCase();
-    return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
+    const normalizedMessage = message.toLowerCase();
+    return normalizedMessage.includes("spam") || normalizedMessage.includes("sale");
 }
 
 console.log(checkForSpam("Latest technology news")); // false
